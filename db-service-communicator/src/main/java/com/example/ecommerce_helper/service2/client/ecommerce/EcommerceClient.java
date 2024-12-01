553cd519adf2ca725759bf2ef8f0bd648f301aab
deepak <darshandeepak1483@gmail.com>
2024-12-01 20:16:24 +0530

    added api gateway still need to fix on issues

--- db-service-communicator/src/main/java/com/example/ecommerce_helper/service2/client/ecommerce/EcommerceClient.java
+++ db-service-communicator/src/main/java/com/example/ecommerce_helper/service2/client/ecommerce/EcommerceClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "ecommerce",url = "http://localhost:8080",path = "/api/product")
+@FeignClient(value = "SERVICE2",url = "http://localhost:8080",path = "/api/product")
 public interface EcommerceClient {
 
     @PostMapping(value = "/create",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
